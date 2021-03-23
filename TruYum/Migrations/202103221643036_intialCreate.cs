6bd3f8e3fb31a2fd14bb01fe5ba94726fba505d8
Mounika Sodum <mounikasodum229@gmail.com>
2021-03-23 10:58:38 +0530

    Add project files.

---
+++ TruYum/Migrations/202103221643036_intialCreate.cs
@@ -0,0 +1,59 @@
+﻿namespace TruYum.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class intialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cart",
+                c => new
+                    {
+                        CartId = c.Int(nullable: false, identity: true),
+                        MenuItemId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.CartId)
+                .ForeignKey("dbo.MenuItem", t => t.MenuItemId, cascadeDelete: true)
+                .Index(t => t.MenuItemId);
+            
+            CreateTable(
+                "dbo.MenuItem",
+                c => new
+                    {
+                        MenuItemId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        price = c.Double(nullable: false),
+                        Active = c.Boolean(nullable: false),
+                        DateOfLaunch = c.DateTime(nullable: false),
+                        freedelivery = c.Boolean(nullable: false),
+                        CategoryId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.MenuItemId)
+                .ForeignKey("dbo.Category", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Category",
+                c => new
+                    {
+                        CategoryId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Cart", "MenuItemId", "dbo.MenuItem");
+            DropForeignKey("dbo.MenuItem", "CategoryId", "dbo.Category");
+            DropIndex("dbo.MenuItem", new[] { "CategoryId" });
+            DropIndex("dbo.Cart", new[] { "MenuItemId" });
+            DropTable("dbo.Category");
+            DropTable("dbo.MenuItem");
+            DropTable("dbo.Cart");
+        }
+    }
+}
---
+++ TruYum/Migrations/202103230442160_intialCreate1.cs
@@ -0,0 +1,16 @@
+namespace TruYum.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class intialCreate1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
